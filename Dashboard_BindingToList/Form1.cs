c2cdf74aefa4b50ee14d3ac018070423da5c825e
Alexgoon <alex.russkovdx@gmail.com>
2017-02-03 15:24:25 +0300

    Fix

--- Dashboard_BindingToList/Form1.cs
+++ Dashboard_BindingToList/Form1.cs
@@ -4,7 +4,7 @@ using DevExpress.XtraEditors;
 namespace Dashboard_BindingToList {
     public partial class Form1 : XtraForm {
         public Form1() {
-            InitializeComponent();          f        
+            InitializeComponent();                
         }
 
         private void Form1_Load(object sender, System.EventArgs e) {
